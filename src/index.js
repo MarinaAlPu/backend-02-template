2e5c30f543ee7c3eaa3bd1232a95d3752735d145
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-03 22:52:20 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,99 +1,49 @@
-const http = require('node:http');
-const getUsers = require('./modules/users');
-require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT;
+const userRouter = require("./routes/users");
+const loggerOne = require("./middlewares/loggerOne");
 
+dotenv.config();
 
-const server = http.createServer((request, response) => {
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200 +
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400 +
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200 +
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200 +
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500 +
+const {
+    PORT = 3000,
+    API_URL = "http://127.0.0.1",
+    MONGO_URL = "mongodb://localhost:27017/backend"
+} = process.env;
 
-    if (request.url === '/favicon.ico') {
-        response.statusCode = 204;
-        response.end();
-        return;
-    }
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        if (error) throw error;
+    });
 
-    if (request.url === "/?users") {
-        response.statusCode = 200;
-        response.statusMessage = "OK";
-        response.setHeader("Content-Type", "application/json");
-        response.write(getUsers());
-        response.end();
-
-        return;
-    };
+const app = express();
 
 
-    const url = new URL(request.url, 'http://127.0.0.1:3003');
-    const params = url.searchParams;
-    const keys = params.keys();
-
-    let paramsNames = [];
-
-    for (let key of keys) {
-        paramsNames.push(key);
-    }
-
-    if (paramsNames.length > 0) {
-        const isParamsNamesWrong = paramsNames.some(paramName => paramName !== "hello");
-
-        if (isParamsNamesWrong) {
-            response.statusCode = 500;
-            response.statusMessage = "Error";
-            // response.setHeader("Content-Type", "text/plain");
-            response.write("Empty");
-            response.end();
-
-            return;
-        }
-
-        if (request.url.includes("hello")) {
-            const url = new URL(request.url, 'http://127.0.0.1:3003');
-            const hasHello = url.searchParams.has('hello');
-            const hello = url.searchParams.get('hello');
-
-            if (hasHello) {
-                if (hello) {
-                    response.statusCode = 200;
-                    response.statusMessage = "OK";
-                    response.setHeader("Content-Type", "text/plain");
-                    response.write(`Hello, ${hello}.`);
-                    response.end();
-
-                    return;
-                }
-
-                if (!hello) {
-                    response.statusCode = 400;
-                    response.statusMessage = "Bad request";
-                    response.setHeader("Content-Type", "text/plain");
-                    response.write("Enter a name");
-                    response.end();
-
-                    return;
-                }
-                return;
-            }
-            return;
-        };
-        return;
-    };
+app.use(cors());
+app.use(loggerOne);
+app.use(bodyParser.json());
 
 
+
+// фукнция обработки данных
+const hello = (request, response) => {
     response.statusCode = 200;
-    response.statusMessage = "OK";
-    response.setHeader("Content-Type", "text/plain");
-    response.write("Hello, World!");
-    response.end();
+    response.send("Hello, GET");
+}
+
+app.get("/", hello);
+
+
+app.use(userRouter);
+
+
+app.listen(`${PORT}`, () => {
+    console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
 });
-
-
-server.listen(PORT, () => {
-    console.log(`Сервер запущен по адресу 127.0.0.1:${PORT}`);
-})
