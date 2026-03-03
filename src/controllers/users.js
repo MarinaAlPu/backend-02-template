b77de6cef6a1aa48baca70acb6f4666d774179ed
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-03 21:56:26 +0500

    Add users controllers

---
+++ src/controllers/users.js
@@ -0,0 +1,28 @@
+const getUsers = (request, response) => {
+  // Get all users
+};
+
+const getUser = (request, response) => {
+  // Get user by ID
+};
+
+const createUser = (request, response) => {
+  // Add new user
+};
+
+const updateUser = (request, response) => {
+  // Update user by ID
+};
+
+const deleteUser = (request, response) => {
+  // Delete user by ID
+};
+
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+}
